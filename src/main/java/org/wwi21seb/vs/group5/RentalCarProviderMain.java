61f5eef8ff3ff93288575d432d193d81ef9aef45
Frecherenkel60 <rieger.kevin01@gmail.com>
2023-07-16 17:04:56 +0200

    sync

--- src/main/java/org/wwi21seb/vs/group5/RentalCarProviderMain.java
+++ src/main/java/org/wwi21seb/vs/group5/RentalCarProviderMain.java
@@ -1,67 +1,18 @@
 package org.wwi21seb.vs.group5;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.wwi21seb.vs.group5.Logger.LoggerFactory;
-import org.wwi21seb.vs.group5.UDP.UDPMessage;
 import org.wwi21seb.vs.group5.service.RentalService;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
 import java.util.logging.Logger;
 
 public class RentalCarProviderMain {
 
     private static final Logger LOGGER = LoggerFactory.setupLogger(RentalCarProviderMain.class.getName());
 
-    public synchronized static void main(String[] args) {
-        LOGGER.info("Starting up!");
-        ObjectMapper mapper = new ObjectMapper();
-
-        // Initialize the RentalService
-        LOGGER.info("Initializing RentalService!");
+    public static void main(String[] args) {
+        LOGGER.info("Starting HotelRoomProvider");
         RentalService rentalService = new RentalService();
-
-        try (DatagramSocket socket = new DatagramSocket(5001)) {
-            LOGGER.info("Socket initialized on port 5001!");
-
-            byte[] buffer = new byte[16384];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-            while (true) {
-                LOGGER.info("Waiting for message!");
-                socket.receive(packet);
-                String message = new String(packet.getData(), 0, packet.getLength());
-
-                UDPMessage parsedMessage = mapper.readValue(message, UDPMessage.class);
-                LOGGER.info(String.format("Received %s message from %s: %s", parsedMessage.getOperation(), parsedMessage.getSender(), parsedMessage.getData()));
-
-                UDPMessage response = null;
-
-                switch (parsedMessage.getOperation()) {
-                    case PREPARE -> response = rentalService.prepare(parsedMessage);
-                    case COMMIT -> response = rentalService.commit(parsedMessage);
-                    case ABORT -> response = rentalService.abort(parsedMessage);
-                    case GET_BOOKINGS -> response = rentalService.getRentals(parsedMessage);
-                    case GET_AVAILABILITY -> response = rentalService.getAvailableRentals(parsedMessage);
-                    default -> LOGGER.severe("Unknown operation received!");
-                }
-
-                if (response != null) {
-                    LOGGER.info(String.format("Sending %s message to %s: %s", response.getOperation(), response.getSender(), response.getData()));
-                    byte[] responseBytes = mapper.writeValueAsBytes(response);
-                    DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, packet.getAddress(), packet.getPort());
-                    socket.send(responsePacket);
-                }
-            }
-        } catch (SocketException e) {
-            LOGGER.severe("Error while initializing socket!");
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            LOGGER.severe("Error while receiving message!");
-            throw new RuntimeException(e);
-        }
+        rentalService.start();
     }
 
 }
--- src/main/java/org/wwi21seb/vs/group5/service/RentalService.java
+++ src/main/java/org/wwi21seb/vs/group5/service/RentalService.java
@@ -6,33 +6,175 @@ import org.wwi21seb.vs.group5.Logger.LoggerFactory;
 import org.wwi21seb.vs.group5.Request.ReservationRequest;
 import org.wwi21seb.vs.group5.Request.TransactionResult;
 import org.wwi21seb.vs.group5.TwoPhaseCommit.*;
+import org.wwi21seb.vs.group5.UDP.Operation;
 import org.wwi21seb.vs.group5.UDP.UDPMessage;
 import org.wwi21seb.vs.group5.dao.RentalDAO;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RentalService {
 
     private static final Logger LOGGER = LoggerFactory.setupLogger(RentalService.class.getName());
+    private static final String CAR_PROIVDER = "CarProvider";
+    private final DatagramSocket socket;
+    private final byte[] buffer = new byte[16384];
     private final ConcurrentHashMap<UUID, ParticipantContext> contexts = new ConcurrentHashMap<>();
     private final LogWriter<ParticipantContext> logWriter = new LogWriter<>();
-    private static final String CAR_PROIVDER = "CarProvider";
     private final RentalDAO rentalDAO;
     private final ObjectMapper mapper;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public RentalService() {
         this.rentalDAO = new RentalDAO();
         this.mapper = new ObjectMapper();
+
+        try {
+            socket = new DatagramSocket(5001);
+            LOGGER.info("Socket initialized on port 5002!");
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
 
         // Restore the state of the service
         // This is done by reading the log file and replaying the transactions
         for (ParticipantContext participantContext : logWriter.readAllLogs()) {
             LOGGER.log(Level.INFO, "Restoring transaction {0}", participantContext.getTransactionId());
             contexts.put(participantContext.getTransactionId(), participantContext);
+
+            // Get participant
+            Participant participant = participantContext.getParticipants().stream().filter(p -> p.getName().equals(CAR_PROIVDER)).findFirst().orElseThrow();
+            UDPMessage response = null;
+
+            switch (participantContext.getTransactionState()) {
+                case PREPARE -> {
+                    if (participant.getVote().equals(Vote.NO)) {
+                        // If the participant voted no, abort the transaction
+                        UDPMessage message = new UDPMessage(Operation.ABORT, participantContext.getTransactionId(), participantContext.getCoordinator().getName(), null);
+                        response = abort(message);
+                    } else {
+                        // If the participant voted yes, we need to ask the coordinator
+                        // for the result of the transaction. This is because we probably
+                        // crashed after voting yes, which is why we didn't receive the
+                        // commit/abort message from the coordinator
+                        response = new UDPMessage(Operation.RESULT, participantContext.getTransactionId(), participant.getName(), null);
+
+                        // Schedule a timeout task to ask the other participants for their vote
+                        // after 10 seconds, since we assume that the coordinator crashed
+                        // We don't need to ask the coordinator for the result again, since the
+                        // coordinator will send the result to us again after it has recovered
+                        askParticipantForDecision(participantContext);
+                    }
+                }
+                case COMMIT -> {
+                    // If the transaction was already committed, commit it again
+                    // The Coordinator will ignore the commit request if the transaction
+                    // was already committed
+                    UDPMessage message = new UDPMessage(Operation.COMMIT, participantContext.getTransactionId(), participantContext.getCoordinator().getName(), null);
+                    response = commit(message);
+                }
+                case ABORT -> {
+                    // If the transaction was already aborted, abort it again
+                    // The Coordinator will ignore the abort request if the transaction
+                    // was already aborted
+                    UDPMessage message = new UDPMessage(Operation.ABORT, participantContext.getTransactionId(), participantContext.getCoordinator().getName(), null);
+                    response = abort(message);
+                }
+            }
+
+            if (response != null) {
+                // Send the response to the coordinator
+                LOGGER.log(Level.INFO, "Restored transaction {0} with response {1}", new Object[]{participantContext.getTransactionId(), response.getOperation()});
+
+                try {
+                    byte[] responseBytes = mapper.writeValueAsBytes(response);
+                    LOGGER.info(participantContext.getCoordinator().toString());
+                    DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, participantContext.getCoordinator().getUrl(), participantContext.getCoordinator().getPort());
+                    LOGGER.info(String.format("Sending %s message to %s: %s", response.getOperation(), participantContext.getCoordinator().getName(), response.getData()));
+                    socket.send(responsePacket);
+                } catch (JsonProcessingException e) {
+                    LOGGER.log(Level.SEVERE, "Failed to serialize response", e);
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    LOGGER.log(Level.SEVERE, "Failed to send response", e);
+                    throw new RuntimeException(e);
+                }
+            } else {
+                LOGGER.log(Level.INFO, "Restored transaction {0}", participantContext.getTransactionId());
+            }
         }
+    }
+
+    public void start() {
+        try {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            while (true) {
+                LOGGER.info("Waiting for message!");
+                socket.receive(packet);
+                String message = new String(packet.getData(), 0, packet.getLength());
+
+                UDPMessage parsedMessage = mapper.readValue(message, UDPMessage.class);
+                UDPMessage response = null;
+                LOGGER.info(String.format("Received %s message from %s: %s", parsedMessage.getOperation(), parsedMessage.getSender(), parsedMessage.getData()));
+
+                switch (parsedMessage.getOperation()) {
+                    case PREPARE -> response = prepare(parsedMessage);
+                    case COMMIT -> response = commit(parsedMessage);
+                    case ABORT -> response = abort(parsedMessage);
+                    case GET_BOOKINGS -> response = getRentals(parsedMessage);
+                    case GET_AVAILABILITY -> response = getAvailableRentals(parsedMessage);
+                    case RESULT -> response = sendResult(parsedMessage);
+                    default -> LOGGER.severe("Unknown operation received!");
+                }
+
+                if (response != null && parsedMessage.getOperation().equals(Operation.RESULT)) {
+                    LOGGER.info(String.format("Sending %s message to %s: %s", response.getOperation(), parsedMessage.getSender(), response.getData()));
+                    byte[] responseBytes = mapper.writeValueAsBytes(response);
+                    DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, packet.getAddress(), packet.getPort());
+                    socket.send(responsePacket);
+                } else if (response != null) {
+                    ParticipantContext participantContext = contexts.get(parsedMessage.getTransactionId());
+
+                    if (participantContext == null) {
+                        // This should not happen, but just in case
+                        LOGGER.log(Level.SEVERE, "No context found for transaction {0}", parsedMessage.getTransactionId());
+                        continue;
+                    }
+
+                    Coordinator coordinator = participantContext.getCoordinator();
+
+                    LOGGER.info(String.format("Sending %s message to %s: %s", response.getOperation(), coordinator.getName(), response.getData()));
+                    byte[] responseBytes = mapper.writeValueAsBytes(response);
+                    DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, coordinator.getUrl(), coordinator.getPort());
+                    socket.send(responsePacket);
+                } else {
+                    LOGGER.info("No response to send!");
+                }
+
+                System.exit(0);
+            }
+        } catch (SocketException e) {
+            LOGGER.severe("Error while initializing socket!");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            LOGGER.severe("Error while receiving message!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void scheduleContextDeletion(UUID transactionId) {
+        scheduler.schedule(() -> {
+            LOGGER.log(Level.INFO, "Deleting transaction {0}", transactionId);
+            logWriter.deleteLog(transactionId);
+            contexts.remove(transactionId);
+        }, 1, TimeUnit.MINUTES);
     }
 
     public UDPMessage prepare(UDPMessage message) {
@@ -99,10 +241,17 @@ public class RentalService {
         // Get the participant from the participantContext
         Participant participant = participantContext.getParticipants().stream().filter(p -> p.getName().equals(CAR_PROIVDER)).findFirst().orElseThrow();
 
+        // Cancel the timeout task
+        CompletableFuture<Boolean> future = participant.getCommitFuture();
+        if (future != null) {
+            future.complete(true);
+        }
+
         if (participant.isDone()) {
             // Double check if the transaction was already committed previously
             // If so, return a TransactionResult with success = true because
             // the transaction was already committed
+            scheduleContextDeletion(participantContext.getTransactionId());
             TransactionResult transactionResult = new TransactionResult(true);
             return getSuccessMessage(message, transactionResult);
         }
@@ -115,6 +264,11 @@ public class RentalService {
             // by setting our participant status to done
             participant.setDone();
         }
+
+        // Set a timer to delete the context after 5 minutes
+        // This is to prevent the contexts map from growing too large
+        // After 5 minutes every participant should have finished its protocol
+        scheduleContextDeletion(participantContext.getTransactionId());
 
         // Update the context in the log
         logWriter.writeLog(participantContext.getTransactionId(), participantContext);
@@ -144,22 +298,33 @@ public class RentalService {
         // Get the participant from the participantContext
         Participant participant = participantContext.getParticipants().stream().filter(p -> p.getName().equals(CAR_PROIVDER)).findFirst().orElseThrow();
 
+        // Cancel the timeout task
+        CompletableFuture<Boolean> future = participant.getCommitFuture();
+        if (future != null) {
+            future.complete(true);
+        }
+
         if (participant.isDone()) {
             // Double check if the transaction was already aborted previously
             // If so, return a TransactionResult with success = true because
             // the transaction was already aborted
+            scheduleContextDeletion(participantContext.getTransactionId());
             TransactionResult transactionResult = new TransactionResult(true);
             return getSuccessMessage(message, transactionResult);
         }
 
         boolean success = rentalDAO.abortRental(participant.getBookingContext().getBookingId());
-        LOGGER.log(Level.INFO, "Abort rental {0}", success);
 
         if (success) {
             // If the commit was successful, we finish the transaction
             // by setting our participant status to done
             participant.setDone();
         }
+
+        // Set a timer to delete the context after 5 minutes
+        // This is to prevent the contexts map from growing too large
+        // After 5 minutes every participant should have finished its protocol
+        scheduleContextDeletion(participantContext.getTransactionId());
 
         // Update the context in the log
         logWriter.writeLog(participantContext.getTransactionId(), participantContext);
@@ -180,6 +345,69 @@ public class RentalService {
         }
 
         return new UDPMessage(message.getOperation(), message.getTransactionId(), CAR_PROIVDER, transactionResultString);
+    }
+
+    public UDPMessage sendResult(UDPMessage message) {
+        ParticipantContext participantContext = contexts.get(message.getTransactionId());
+
+        if (participantContext == null) {
+            // If the participantContext is null, the transaction is unknown to our service
+            // This is either because we weren't available in the prepare phase or because
+            // we already deleted the context
+            return null;
+        }
+
+        // Get the transaction state from the participantContext
+        TransactionState transactionState = participantContext.getTransactionState();
+        UDPMessage udpMessage = null;
+
+        if (transactionState == TransactionState.COMMIT) {
+            udpMessage = new UDPMessage(Operation.COMMIT, message.getTransactionId(), CAR_PROIVDER, null);
+        } else if (transactionState == TransactionState.ABORT) {
+            udpMessage = new UDPMessage(Operation.ABORT, message.getTransactionId(), CAR_PROIVDER, null);
+        } else {
+            // We can't send a result if we don't know the transaction result
+            return null;
+        }
+
+        return udpMessage;
+    }
+
+    public void askParticipantForDecision(ParticipantContext participantContext) {
+        // Get the participant from the participantContext
+        Participant participant = participantContext.getParticipants().stream().filter(p -> p.getName().equals(CAR_PROIVDER)).findFirst().orElseThrow();
+
+        participant.resetCommitFuture();
+        CompletableFuture<Boolean> commitFuture = participant.getCommitFuture();
+        // After 10 seconds of no response, we assume the coordinator crashed
+        // and ask the other participants for the result of the transaction
+        commitFuture.orTimeout(10, TimeUnit.SECONDS).exceptionally(throwable -> {
+            LOGGER.log(Level.WARNING, "Coordinator crashed, asking other participants for result of transaction {0}", participantContext.getTransactionId());
+            for (Participant p : participantContext.getParticipants()) {
+                if (!p.getName().equals(CAR_PROIVDER)) {
+                    UDPMessage resultRequest = new UDPMessage(Operation.RESULT, participantContext.getTransactionId(), CAR_PROIVDER, null);
+                    DatagramPacket resultRequestPacket = new DatagramPacket(buffer, buffer.length, p.getUrl(), p.getPort());
+                    try {
+                        byte[] resultRequestBytes = mapper.writeValueAsBytes(resultRequest);
+                        resultRequestPacket.setData(resultRequestBytes);
+                        socket.send(resultRequestPacket);
+                    } catch (JsonProcessingException e) {
+                        LOGGER.log(Level.SEVERE, "Error while serializing message", e);
+                    } catch (IOException e) {
+                        LOGGER.log(Level.SEVERE, "Error while sending message", e);
+                    }
+
+                    // Trigger the askParticipantForDecision method to invoke another timeout
+                    // in case the participant also crashed
+                    askParticipantForDecision(participantContext);
+                }
+            }
+
+            return null;
+        });
+
+        // Update the context in the log
+        logWriter.writeLog(participantContext.getTransactionId(), participantContext);
     }
 
     /**
